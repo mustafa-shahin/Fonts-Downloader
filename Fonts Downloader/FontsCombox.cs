8e959a01b294d8695c40a01b55992b4f283d6ce6
mustafa-shahin <mustafashahin988@gmail.com>
2023-06-29 12:11:34 +0200

    Revert "Renaming"
    This reverts commit a08a59bfd8798182ab7d6afe4c9ac3575f5a5abc.

--- Fonts Downloader/FontsCombox.cs
+++ Fonts Downloader/FontsCombox.cs
@@ -7,61 +7,243 @@ namespace Fonts_Downloader
 {
     public class FontsCombox
     {
-        private List<string> links100 = new List<string>();
-        public List<string> Links100 { get => links100; set => links100 = value; }
+        private List<string> links_100 = new List<string>();
+        public List<string> Links_100
+        {
+            get
+            {
+                return this.links_100;
+            }
+            set
+            {
+                this.links_100 = value;
+            }
+        }
 
-        private List<string> links100Italic = new List<string>();
-        public List<string> Links100Italic { get => links100Italic; set => links100Italic = value; }
+        private List<string> links_100italic = new List<string>();
+        public List<string> Links_100italic
+        {
+            get
+            {
+                return this.links_100italic;
+            }
+            set
+            {
+                this.links_100italic = value;
+            }
+        }
 
-        private List<string> links200 = new List<string>();
-        public List<string> Links200 { get => links200; set => links200 = value; }
+        private List<string> links_200 = new List<string>();
+        public List<string> Links_200
+        {
+            get
+            {
+                return links_200;
+            }
+            set
+            {
+                this.links_200 = value;
+            }
+        }
 
-        private List<string> links200Italic = new List<string>();
-        public List<string> Links200Italic { get => links200Italic; set => links200Italic = value; }
+        private List<string> links_200italic = new List<string>();
+        public List<string> Links_200italic
+        {
+            get
+            {
+                return links_200italic;
+            }
+            set
+            {
+                this.links_200italic = value;
+            }
+        }
 
-        private List<string> links300 = new List<string>();
-        public List<string> Links300 { get => links300; set => links300 = value; }
+        private List<string> links_300 = new List<string>();
+        public List<string> Links_300
+        {
+            get
+            {
+                return links_300;
+            }
+            set
+            {
+                this.links_300 = value;
+            }
+        }
 
-        private List<string> links300Italic = new List<string>();
-        public List<string> Links300Italic { get => links300Italic; set => links300Italic = value; }
+        private List<string> links_300italic = new List<string>();
 
-        private List<string> links400 = new List<string>();
-        public List<string> Links400 { get => links400; set => links400 = value; }
+        public List<string> Links_300italic
+        {
+            get
+            {
+                return links_300italic;
+            }
+            set
+            {
+                this.links_300italic = value;
+            }
+        }
 
-        private List<string> links400Italic = new List<string>();
-        public List<string> Links400Italic { get => links400Italic; set => links400Italic = value; }
+        private List<string> links_regular = new List<string>();
 
-        private List<string> links500 = new List<string>();
-        public List<string> Links500 { get => links500; set => links500 = value; }
+        public List<string> Links_regular
+        {
+            get
+            {
+                return links_regular;
+            }
+            set
+            {
+                this.links_regular = value;
+            }
+        }
 
-        private List<string> link500Italic = new List<string>();
-        public List<string> Links500Italic { get => link500Italic; set => link500Italic = value; }
+        private List<string> links_italic = new List<string>();
+        public List<string> Links_italic
+        {
+            get
+            {
+                return this.links_italic;
+            }
+            set
+            {
+                this.links_italic = value;
+            }
+        }
 
-        private List<string> links600 = new List<string>();
-        public List<string> Links600 { get => links600; set => links600 = value; }
+        private List<string> links_500 = new List<string>();
+        public List<string> Links_500
+        {
+            get
+            {
+                return links_500;
+            }
+            set
+            {
+                this.links_500 = value;
+            }
+        }
 
-        private List<string> links600Italic = new List<string>();
-        public List<string> Links600Italic { get => links600Italic; set => links600Italic = value; }
+        private List<string> links_500italic = new List<string>();
+        public List<string> Links_500italic
+        {
+            get
+            {
+                return links_500italic;
+            }
+            set
+            {
+                this.links_500italic = value;
+            }
+        }
 
 
-        private List<string> links700 = new List<string>();
-        public List<string> Links700 { get => links700; set => links700 = value; }
+        private List<string> links_600 = new List<string>();
+        public List<string> Links_600
+        {
+            get
+            {
+                return links_600;
+            }
+            set
+            {
+                this.links_600 = value;
+            }
+        }
 
-        private List<string> links700Italic = new List<string>();
-        public List<string> Links700Italic { get => links700Italic; set => links700Italic = value; }
+        private List<string> links_600italic = new List<string>();
+        public List<string> Links_600italic
+        {
+            get
+            {
+                return links_600italic;
+            }
+            set
+            {
+                this.links_600italic = value;
+            }
+        }
 
-        private List<string> links800 = new List<string>();
-        public List<string> Links800 { get => links800; set => links800 = value; }
+        private List<string> links_700 = new List<string>();
+        public List<string> Links_700
+        {
+            get
+            {
+                return links_700;
+            }
+            set
+            {
+                this.links_700 = value;
+            }
+        }
 
-        private List<string> links800Italic = new List<string>();
-        public List<string> Links800Italic { get => links800Italic; set => links800Italic = value; }
+        private List<string> links_700italic = new List<string>();
+        public List<string> Links_700italic
+        {
+            get
+            {
+                return links_700italic;
+            }
+            set
+            {
+                this.links_700italic = value;
+            }
+        }
 
-        private List<string> links900 = new List<string>();
-        public List<string> Links900 { get => links900; set => links900 = value; }
+        private List<string> links_800 = new List<string>();
+        public List<string> Links_800
+        {
+            get
+            {
+                return links_800;
+            }
+            set
+            {
+                this.links_800 = value;
+            }
+        }
 
-        private List<string> links900Italic = new List<string>();
-        public List<string> Links900Italic { get => links700Italic; set => links900Italic = value; }
-       
+        private List<string> links_800italic = new List<string>();
+        public List<string> Links_800italic
+        {
+            get
+            {
+                return links_800italic;
+            }
+            set
+            {
+                this.links_800italic = value;
+            }
+        }
+
+        private List<string> links_900 = new List<string>();
+        public List<string> Links_900
+        {
+            get
+            {
+                return links_900;
+            }
+            set
+            {
+                this.links_900 = value;
+            }
+        }
+
+        private List<string> links_900italic = new List<string>();
+        public List<string> Links_900italic
+        {
+            get
+            {
+                return links_900italic;
+            }
+            set
+            {
+                this.links_900italic = value;
+            }
+        }
+
         private List<Item> allList = new List<Item>();
         private List<string> Subset = new List<string>();
         public List<Item> AllList { get { return allList; } }
@@ -77,66 +259,60 @@ namespace Fonts_Downloader
                 foreach (var list in FontResponse.items)
                 {
                     allList.Add(list);
-                    
-                    /*+*+*+*+*+*+*+*+*+*Normal Fonts*+*+*+*+*+*+*+*+*+*/
                     if (!string.IsNullOrEmpty(list.files._100))
-                        links100.Add(list.files._100);
+                        links_100.Add(list.files._100);
+
+                    if (!string.IsNullOrEmpty(list.files._100italic))
+                        links_100italic.Add(list.files._100italic);
 
                     if (!string.IsNullOrEmpty(list.files._200))
-                        links200.Add(list.files._200);
+                        links_200.Add(list.files._200);
+
+                    if (!string.IsNullOrEmpty(list.files._200italic))
+                        links_200italic.Add(list.files._200italic);
 
                     if (!string.IsNullOrEmpty(list.files._300))
-                        links300.Add(list.files._300);
-
-                    if (!string.IsNullOrEmpty(list.files.regular))
-                        links400.Add(list.files.regular);
-
-                    if (!string.IsNullOrEmpty(list.files._500))
-                        links500.Add(list.files._500);
-
-                    if (!string.IsNullOrEmpty(list.files._600))
-                        links600.Add(list.files._600);
-
-                    if (!string.IsNullOrEmpty(list.files._700))
-                        links700.Add(list.files._700);
-
-                    if (!string.IsNullOrEmpty(list.files._800))
-                        links800.Add(list.files._800);
-
-                    if (!string.IsNullOrEmpty(list.files._900))
-                        links900.Add(list.files._900);
-
-
-                    /*+*+*+*+*+*+*+*+*+*Italic Fonts*+*+*+*+*+*+*+*+*+*/
-                    if (!string.IsNullOrEmpty(list.files._100italic))
-                        links100Italic.Add(list.files._100italic);
-                    
-                    if (!string.IsNullOrEmpty(list.files._200italic))
-                        links200Italic.Add(list.files._200italic);
+                        links_300.Add(list.files._300);
 
                     if (!string.IsNullOrEmpty(list.files._300italic))
-                        links300Italic.Add(list.files._300italic);
+                        links_300italic.Add(list.files._300italic);
+
+                    if (!string.IsNullOrEmpty(list.files.regular))
+                        links_regular.Add(list.files.regular);
 
                     if (!string.IsNullOrEmpty(list.files.italic))
-                        links400Italic.Add(list.files.italic);
+                        links_italic.Add(list.files.italic);
+
+                    if (!string.IsNullOrEmpty(list.files._500))
+                        links_500.Add(list.files._500);
 
                     if (!string.IsNullOrEmpty(list.files._500italic))
-                        link500Italic.Add(list.files._500italic);
+                        links_500italic.Add(list.files._500italic);
+
+                    if (!string.IsNullOrEmpty(list.files._600))
+                        links_600.Add(list.files._600);
 
                     if (!string.IsNullOrEmpty(list.files._600italic))
-                        links600Italic.Add(list.files._600italic);
+                        links_600italic.Add(list.files._600italic);
+
 
                     if (!string.IsNullOrEmpty(list.files._700))
-                        links700.Add(list.files._700);
+                        links_700.Add(list.files._700);
+
+                    if (!string.IsNullOrEmpty(list.files._700italic))
+                        links_700italic.Add(list.files._700italic);
 
                     if (!string.IsNullOrEmpty(list.files._800))
-                        links800.Add(list.files._800);
+                        links_800.Add(list.files._800);
+
+                    if (!string.IsNullOrEmpty(list.files._800italic))
+                        links_800italic.Add(list.files._800italic);
 
                     if (!string.IsNullOrEmpty(list.files._900))
-                        links900.Add(list.files._900);
+                        links_900.Add(list.files._900);
 
                     if (!string.IsNullOrEmpty(list.files._900italic))
-                        links900Italic.Add(list.files._900italic);
+                        links_900italic.Add(list.files._900italic);
 
                 }
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
