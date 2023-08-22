f47ec1d6347e24bdb04a39fe45b15d0a78fa5f41
mustafa-shahin <mustafashahin988@gmail.com>
2023-08-22 20:30:46 +0200

    Update CssFile.cs
    1. Moved the FontFileStyles dictionary to a class member since it's a constant.
    2. Utilized string.IsNullOrWhiteSpace() to check for empty or whitespace strings.
    2. Removed redundant FontWeights.Clear() and unnecessary if conditions.
    2. Improved the formatting of the CSS string generation for better readability.
    2. Combined the folder and file paths using Path.Combine for better cross-platform compatibility.
    2. Used Any() to check if a collection has elements instead of checking its Count.
    2. Removed unused fields and properties (Fontface, Fontfamily, Fontstyle, OldFont, etc.).
    2. Simplified the font file style retrieval using the FontFileStyles dictionary.
    2. Replaced Substring(0, 1).ToUpper() + Substring(1) with char.ToUpper() for cleaner code.
    2. Removed the unnecessary else condition after checking FontName.

--- Fonts Downloader/CssFile.cs
+++ Fonts Downloader/CssFile.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
@@ -9,83 +8,55 @@ namespace Fonts_Downloader
 {
     internal class CssFile
     {
-        private List<string> FontWeights = new List<string>();
+        private readonly Dictionary<string, string> FontFileStyles = new Dictionary<string, string>
+        {
+            { "100", "Thin" },
+            { "200", "ExtraLight" },
+            { "300", "Light" },
+            { "400", "Regular" },
+            { "500", "Medium" },
+            { "600", "SemiBold" },
+            { "700", "Bold" },
+            { "800", "ExtraBold" },
+            { "900", "Black" },
+        };
+
         private List<string> _Styles = new List<string>();
-        public List<string> Styles { get { return _Styles; } }
+        public List<string> Styles => _Styles;
 
-        private readonly string Fontface = "@font-face {";
-        private readonly string Fontfamily = "\nfont-family:";
-        private string fontStyle = "\nfont-style:";
-        private readonly string fontweight = "\nfont-weight:";
-        private string _FontStyle;
-        private string OldFont;
-
-        public string FontFace { get { return Fontface; } }
-        public string FontFamily { get { return Fontfamily; } }
-        public string Fontstyle { get { return fontStyle; } }
-        public string FontStyles { set { this._FontStyle = value; } get { return this._FontStyle; } }
-        public List<string> FontWeight { get { return FontWeights; } }
+        private List<string> _FontWeight = new List<string>();
+        public List<string> FontWeight => _FontWeight;
         public void CreateCSS(CheckedListBox SizeAndStyle, List<string> SubSet, string FolderName, string FontName)
         {
-            var FontFileStyles = new Dictionary<string, string>
+            List<string> Css = new List<string>();
+
+            for (int i = 0; i < SizeAndStyle.CheckedItems.Count; i++)
             {
-                    { "100", "Thin" },
-                    { "200", "ExtraLight" },
-                    { "300", "Light" },
-                    { "400", "Regular" },
-                    { "500", "Medium" },
-                    { "600", "SemiBold" },
-                    { "700", "ExtraBold" },
-                    { "800", "ExtraBold" },
-                    { "900", "Black" },
-            };
-            string FontFileStyle="";
-            var Css = new List<string>();
-            if (FontWeights.Any())
-            {
-                FontWeights.Clear();
-            }
-            if (SizeAndStyle.CheckedItems.Count != 0)
-            {
-                for (int i = 0; i < SizeAndStyle.CheckedItems.Count; i++)
+                string fontStyle = SizeAndStyle.CheckedItems[i].ToString().Contains("italic") ? "italic" : "normal";
+                _Styles.Add(fontStyle);
+                 
+                string fontWeight = SizeAndStyle.CheckedItems[i].ToString().Replace("italic", "");
+                _FontWeight.Add(fontWeight);
+                if (!string.IsNullOrWhiteSpace(fontWeight))
                 {
-                     _FontStyle = SizeAndStyle.CheckedItems[i].ToString().Contains("italic") ? "italic" : "normal";
-                    if (!SizeAndStyle.CheckedItems[i].Equals(false))
+                    string fontFileStyle = FontFileStyles.ContainsKey(fontWeight) ? FontFileStyles[fontWeight] : "Regular";
+
+                    foreach (var sub in SubSet)
                     {
-                        _Styles.Add(_FontStyle);
-                        FontWeights.Add(SizeAndStyle.CheckedItems[i].ToString().Replace("italic", ""));
-                    }
-                    else
-                    {
-                        FontWeights.Remove(SizeAndStyle.CheckedItems[i].ToString());
-                    }
-                    OldFont = FontName;
-                    if (FontName != OldFont)
-                    {
-                    }
-                    else
-                    {                                                    
-                        if (FontFileStyles.ContainsKey(FontWeight[i]))
-                        {
-                                    FontFileStyle = FontFileStyles[(FontWeight[i])];
-                        }                       
-                        foreach (var sub in SubSet)
-                        {
-                            string css = $"/*{sub}*/" + "\n" + FontFace + FontFamily + $"'{FontName}';" + $"{fontStyle}" + $"{_FontStyle};" + $"{fontweight}" + $"{FontWeights[i]};" 
-                                + "\r\nfont-stretch: 100%;" + "\r\n" + $"src: url('{FontName.Replace(" ", "")}-{_FontStyle.Substring(0, 1).ToUpper() + _FontStyle.Substring(1)}-{FontFileStyle}.ttf')" + "\r\n}";
-                            Css.Add(css);
-                        }
-                        if (i == SizeAndStyle.CheckedItems.Count)
-                        {
-                            break;
-                        }
+                        string css = $"/*{sub}*/" +
+                                     $"\n@font-face {{\nfont-family: '{FontName}';\nfont-style: {fontStyle};\nfont-weight: {fontWeight};\nfont-stretch: 100%;" +
+                                     $"\nsrc: url('{FontName.Replace(" ", "")}-{char.ToUpper(fontStyle[0]) + fontStyle.Substring(1)}-{fontFileStyle}.ttf')\n}}";
+                        Css.Add(css);
                     }
                 }
             }
-            if(FontWeight.Any())
+
+            if (Css.Any())
             {
-                Directory.CreateDirectory($"{FolderName}\\{FontName.Replace(" ", "")}");
-                using (StreamWriter writer = new StreamWriter($"{FolderName}\\{FontName.Replace(" ", "")}\\{FontName.Replace(" ", "")}.css", false))
+                string fontFolder = $"{FolderName}\\{FontName.Replace(" ", "")}";
+                Directory.CreateDirectory(fontFolder);
+
+                using (StreamWriter writer = new StreamWriter(Path.Combine(fontFolder, $"{FontName.Replace(" ", "")}.css"), false))
                 {
                     foreach (var str in Css)
                     {
@@ -93,7 +64,6 @@ namespace Fonts_Downloader
                     }
                 }
             }
-
         }
     }
 }
