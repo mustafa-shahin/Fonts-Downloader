bffaf021d44a16a961f5342df5977c73adbb99d5
Mustafa Shahin <mustafashahin988@gmail.com>
2023-12-06 00:07:28 +0100

    renaming a control and adding a image control for no internet status

--- Fonts Downloader/MainForm.Designer.cs
+++ Fonts Downloader/MainForm.Designer.cs
@@ -37,7 +37,7 @@ namespace Fonts_Downloader
             SelectFolder = new Button();
             SelectedFolder = new TextBox();
             FontBox1 = new ComboBox();
-            SelectedFont = new Label();
+            SelectedFontLabel = new Label();
             CopyFont = new Button();
             webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
             LeftPanel = new Panel();
@@ -53,6 +53,7 @@ namespace Fonts_Downloader
             GitPic = new PictureBox();
             GitHubLink = new LinkLabel();
             WebPanel = new Panel();
+            NoInternet = new PictureBox();
             Header = new Panel();
             TitlePanel = new Panel();
             pictureBox1 = new PictureBox();
@@ -69,6 +70,7 @@ namespace Fonts_Downloader
             GitPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)GitPic).BeginInit();
             WebPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)NoInternet).BeginInit();
             Header.SuspendLayout();
             TitlePanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
@@ -137,17 +139,17 @@ namespace Fonts_Downloader
             FontBox1.Text = "Selected Font";
             FontBox1.SelectionChangeCommitted += FontBox1_SelectionChangeCommitted;
             // 
-            // SelectedFont
+            // SelectedFontLabel
             // 
-            SelectedFont.AutoSize = true;
-            SelectedFont.FlatStyle = FlatStyle.Flat;
-            SelectedFont.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            SelectedFont.ForeColor = Color.FromArgb(185, 185, 185);
-            SelectedFont.Location = new Point(17, 64);
-            SelectedFont.Name = "SelectedFont";
-            SelectedFont.Size = new Size(51, 25);
-            SelectedFont.TabIndex = 33;
-            SelectedFont.Text = "Font";
+            SelectedFontLabel.AutoSize = true;
+            SelectedFontLabel.FlatStyle = FlatStyle.Flat;
+            SelectedFontLabel.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            SelectedFontLabel.ForeColor = Color.FromArgb(185, 185, 185);
+            SelectedFontLabel.Location = new Point(17, 64);
+            SelectedFontLabel.Name = "SelectedFontLabel";
+            SelectedFontLabel.Size = new Size(51, 25);
+            SelectedFontLabel.TabIndex = 33;
+            SelectedFontLabel.Text = "Font";
             // 
             // CopyFont
             // 
@@ -181,7 +183,6 @@ namespace Fonts_Downloader
             webView21.Size = new Size(1111, 757);
             webView21.TabIndex = 35;
             webView21.ZoomFactor = 1D;
-            webView21.CoreWebView2InitializationCompleted += WebView21_CoreWebView2InitializationCompleted;
             // 
             // LeftPanel
             // 
@@ -192,7 +193,7 @@ namespace Fonts_Downloader
             LeftPanel.Controls.Add(CopyFont);
             LeftPanel.Controls.Add(SubsetsLabel);
             LeftPanel.Controls.Add(WOFF2);
-            LeftPanel.Controls.Add(SelectedFont);
+            LeftPanel.Controls.Add(SelectedFontLabel);
             LeftPanel.Controls.Add(SubsetsLists);
             LeftPanel.Controls.Add(SizeAndStyle);
             LeftPanel.Controls.Add(FontBox1);
@@ -208,7 +209,7 @@ namespace Fonts_Downloader
             Minify.AutoSize = true;
             Minify.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
             Minify.ForeColor = Color.FromArgb(185, 185, 185);
-            Minify.Location = new Point(17, 729);
+            Minify.Location = new Point(17, 772);
             Minify.Margin = new Padding(3, 5, 3, 5);
             Minify.Name = "Minify";
             Minify.Size = new Size(136, 29);
@@ -230,7 +231,7 @@ namespace Fonts_Downloader
             TTF.TabIndex = 53;
             TTF.Text = "TTF";
             TTF.UseVisualStyleBackColor = true;
-            TTF.CheckedChanged += TTF_CheckedChanged;
+            TTF.CheckedChanged += TTF_Checked;
             // 
             // SubsetsLabel
             // 
@@ -257,7 +258,7 @@ namespace Fonts_Downloader
             WOFF2.TabIndex = 52;
             WOFF2.Text = "WOFF2";
             WOFF2.UseVisualStyleBackColor = true;
-            WOFF2.CheckedChanged += WOFF2_CheckedChanged;
+            WOFF2.CheckedChanged += WOFF2_Checked;
             // 
             // SubsetsLists
             // 
@@ -270,8 +271,9 @@ namespace Fonts_Downloader
             SubsetsLists.Location = new Point(17, 465);
             SubsetsLists.Margin = new Padding(3, 5, 3, 2);
             SubsetsLists.Name = "SubsetsLists";
-            SubsetsLists.Size = new Size(237, 250);
+            SubsetsLists.Size = new Size(237, 300);
             SubsetsLists.TabIndex = 50;
+            SubsetsLists.SelectedIndexChanged += SubsetsLists_SelectedIndexChanged;
             // 
             // TopPanel
             // 
@@ -332,7 +334,7 @@ namespace Fonts_Downloader
             GitPic.SizeMode = PictureBoxSizeMode.AutoSize;
             GitPic.TabIndex = 54;
             GitPic.TabStop = false;
-            GitPic.Click += GitPic_Click;
+            GitPic.Click += ShowGitRepo;
             // 
             // GitHubLink
             // 
@@ -349,12 +351,13 @@ namespace Fonts_Downloader
             GitHubLink.TabStop = true;
             GitHubLink.Text = "Code";
             GitHubLink.VisitedLinkColor = Color.White;
-            GitHubLink.Click += GitHubLink_Click;
+            GitHubLink.Click += ShowGitRepo;
             // 
             // WebPanel
             // 
             WebPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             WebPanel.BackColor = Color.FromArgb(32, 33, 36);
+            WebPanel.Controls.Add(NoInternet);
             WebPanel.Controls.Add(webView21);
             WebPanel.ForeColor = Color.FromArgb(142, 181, 245);
             WebPanel.Location = new Point(293, 165);
@@ -362,6 +365,17 @@ namespace Fonts_Downloader
             WebPanel.Name = "WebPanel";
             WebPanel.Size = new Size(1111, 757);
             WebPanel.TabIndex = 38;
+            // 
+            // NoInternet
+            // 
+            NoInternet.Image = (Image)resources.GetObject("NoInternet.Image");
+            NoInternet.Location = new Point(338, 228);
+            NoInternet.Name = "NoInternet";
+            NoInternet.Size = new Size(469, 314);
+            NoInternet.SizeMode = PictureBoxSizeMode.Zoom;
+            NoInternet.TabIndex = 36;
+            NoInternet.TabStop = false;
+            NoInternet.Visible = false;
             // 
             // Header
             // 
@@ -374,7 +388,7 @@ namespace Fonts_Downloader
             Header.Size = new Size(1416, 58);
             Header.TabIndex = 39;
             Header.MouseDown += Header_MouseDown;
-            Header.MouseMove += Header_MouseMove;
+            Header.MouseMove += MouseMove;
             Header.MouseUp += Header_MouseUp;
             // 
             // TitlePanel
@@ -384,29 +398,32 @@ namespace Fonts_Downloader
             TitlePanel.Dock = DockStyle.Left;
             TitlePanel.Location = new Point(0, 0);
             TitlePanel.Name = "TitlePanel";
-            TitlePanel.Size = new Size(346, 58);
+            TitlePanel.Size = new Size(289, 58);
             TitlePanel.TabIndex = 2;
+            TitlePanel.MouseMove += MouseMove;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Location = new Point(12, 0);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(61, 58);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 56;
             pictureBox1.TabStop = false;
+            pictureBox1.MouseMove += MouseMove;
             // 
             // Titel
             // 
             Titel.AutoSize = true;
             Titel.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
             Titel.ForeColor = Color.FromArgb(185, 185, 185);
-            Titel.Location = new Point(67, 12);
+            Titel.Location = new Point(82, 17);
             Titel.Name = "Titel";
             Titel.Size = new Size(199, 31);
             Titel.TabIndex = 1;
             Titel.Text = "Fonts Downloader";
+            Titel.MouseMove += MouseMove;
             // 
             // panel1
             // 
@@ -501,6 +518,7 @@ namespace Fonts_Downloader
             GitPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)GitPic).EndInit();
             WebPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)NoInternet).EndInit();
             Header.ResumeLayout(false);
             TitlePanel.ResumeLayout(false);
             TitlePanel.PerformLayout();
@@ -515,7 +533,7 @@ namespace Fonts_Downloader
         private Button SelectFolder;
         private TextBox SelectedFolder;
         private ComboBox FontBox1;
-        private Label SelectedFont;
+        private Label SelectedFontLabel;
         private Button CopyFont;
         private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
         private Panel LeftPanel;
@@ -531,7 +549,7 @@ namespace Fonts_Downloader
         private LinkLabel GitHubLink;
         private Panel GitPanel;
         private Panel Header;
-        private Button Close;
+        private new Button Close;
         private Button Minimize;
         private Panel panel1;
         private Panel TextBoxPanel;
@@ -540,5 +558,6 @@ namespace Fonts_Downloader
         private Label Titel;
         private Panel TitlePanel;
         private PictureBox pictureBox1;
+        private PictureBox NoInternet;
     }
 }
