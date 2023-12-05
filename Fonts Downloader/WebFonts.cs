527de5777db20a058a20ee5bddf399659b203b83
Mustafa Shahin <mustafashahin988@gmail.com>
2023-12-06 00:04:27 +0100

    Renaming

--- Fonts Downloader/WebFonts.cs
+++ Fonts Downloader/WebFonts.cs
@@ -8,9 +8,9 @@ using System.Windows.Forms;
 
 namespace Fonts_Downloader
 {
-    public class FontsComboBox
+    public class WebFonts
     {       
-        public static async Task<List<Item>> GetWebFontsAsync(string apiKey, bool Woff2)
+        public async Task<List<Item>> GetWebFontsAsync(string apiKey, bool Woff2)
         {
             var FontsList = new List<Item>();
             try
