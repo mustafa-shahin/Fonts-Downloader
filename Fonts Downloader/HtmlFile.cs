f0c2289e06d1981650de50c5a299cd30b9fab3ca
Mustafa Shahin <mustafashahin988@gmail.com>
2023-12-26 16:51:02 +0100

    using StringBuilder instead of list

--- Fonts Downloader/HtmlFile.cs
+++ Fonts Downloader/HtmlFile.cs
@@ -64,13 +64,11 @@ namespace Fonts_Downloader
             string fontFamilyStyle = $"font-family: '{selectedFont}', sans-serif;";
             string bodyStyle = $"body{{\nbackground: #212124;\n{fontFamilyStyle}\ncolor: #b9b9b9;\n}}";
 
-            var pTagCSS = new List<string>();
-            var pTags = new List<string>();
-            var h1Tags = new List<string>();
-            var h1TagsCSS = new List<string>();
+            var tags = new StringBuilder();
+            var styles = new StringBuilder();
 
-            GenerateTags("normal", normalVariants, selectedFont, pTagCSS, pTags, h1TagsCSS, h1Tags);
-            GenerateTags("italic", italicVariants, selectedFont, pTagCSS, pTags, h1TagsCSS, h1Tags);
+            GenerateTags("normal", normalVariants, selectedFont,styles,tags);
+            GenerateTags("italic", italicVariants, selectedFont,styles,tags);
 
             using (StreamWriter writer = new($"{Path}\\index.html", false))
             {
@@ -84,53 +82,26 @@ namespace Fonts_Downloader
                 if (!string.IsNullOrEmpty(normals))
                     htmlContent.AppendLine(googleFontLinkNormals);
 
-                htmlContent.AppendLine("</head>");
-                htmlContent.AppendLine($"<style>\n{bodyStyle}");
-
-                for (int i = 0; i < pTagCSS.Count; i++)
-                {
-                    htmlContent.AppendLine(pTagCSS[i]);
-                    htmlContent.AppendLine(h1TagsCSS[i]);
-
-                    if (i == pTagCSS.Count - 1)
-                    {
-                        htmlContent.AppendLine(".separator{background: #b9b9b9;\r\nheight: 5px;\r\nborder-radius: 5px;}");
-                        htmlContent.AppendLine("</style>");
-                    }
-                }
-                htmlContent.AppendLine("<body>");
-                foreach (var (h1Tag, pTag) in h1Tags.Zip(pTags, (h1, p) => (h1, p)))
-                {                
-                    htmlContent.AppendLine($"{h1Tag}\n{pTag}");
-                    htmlContent.AppendLine("<div class=\"separator\"></div>");
-                }
-
-                htmlContent.AppendLine("</body>\n</html>");
-
-
+                htmlContent.AppendLine($"</head>\n<style>\n{bodyStyle}\n{styles.ToString()}")
+                            .AppendLine(".separator{background: #b9b9b9;\r\nheight: 5px;\r\nborder-radius: 5px;}\n</style>\n<body>")
+                            .AppendLine($"{tags.ToString()}\n</body>\n</html>");
                 writer.Write(htmlContent.ToString());
+                writer.Dispose();
             }
             italicVariants.Clear();
             normalVariants.Clear();
         }
-
-        private static void GenerateTags(string variantType, List<string> variants, string selectedFont, List<string> pTagCSS, List<string> pTags, List<string> h1TagsCSS, List<string> h1Tags)
+        private static void GenerateTags(string variantType, List<string> variants, string selectedFont, StringBuilder styles, StringBuilder tags)
         {
             var FontFileStyles = new FontFileStyles();
             foreach (string variant in variants)
             {
                 var fontFileStyle = FontFileStyles.GetFontFileStyles(variant.Replace("1,", ""));
-                string pTagStyle = $"\np.size{variant.Replace("1,", "")}{variantType}{{\nfont-family: '{selectedFont}';\nfont-style: {variantType};\nfont-weight: {variant.Replace("1,", "")};\nfont-stretch: 100%;}}";
-                pTagCSS.Add(pTagStyle);
-
-                string paragraph = $"<p class = 'size{variant.Replace("1,", "")}{variantType}'>\n{LoremIpsum}\n</p>";
-                pTags.Add(paragraph);
-
-                string h1TagStyle = $"\nh1.size{variant.Replace("1,", "")}{variantType}{{\nfont-family: '{selectedFont}';\nfont-style: {variantType};\nfont-weight: {variant.Replace("1,", "")};\n font-stretch: 100% \n}}";
-                h1TagsCSS.Add(h1TagStyle);
-
-                string h1Tag = $"<h1 class = 'size{variant.Replace("1,", "")}{variantType}'> \n{selectedFont} {variant.Replace("1,", "")} - {fontFileStyle}\n</h1>";
-                h1Tags.Add(h1Tag);
+                tags.AppendLine($"<h1 class = 'size{variant.Replace("1,", "")}{variantType}'> \n{selectedFont} {variant.Replace("1,", "")} - {variantType} - {fontFileStyle}\n</h1>")
+                    .AppendLine($"<p class = 'size{variant.Replace("1,", "")}{variantType}'>\n{LoremIpsum}\n</p>")
+                    .AppendLine("<div class=\"separator\"></div>");
+                styles.AppendLine($"\nh1.size{variant.Replace("1,", "")}{variantType}{{\nfont-family: '{selectedFont}';\nfont-style: {variantType};\nfont-weight: {variant.Replace("1,", "")};\n font-stretch: 100% \n}}")
+                      .AppendLine($"\np.size{variant.Replace("1,", "")}{variantType}{{\nfont-family: '{selectedFont}';\nfont-style: {variantType};\nfont-weight: {variant.Replace("1,", "")};\nfont-stretch: 100%;}}");
             }
         }
     }
